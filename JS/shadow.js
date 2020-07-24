bfe6cda1395394d7f391b22e07359a83f78d4158
syed haider imam abidi <abidihaider89@gmail.com>
2020-07-24 21:19:05 +0500

    initialProjectCompletion

--- JS/shadow.js
+++ JS/shadow.js
@@ -1,9 +1,27 @@
 const mainDiv = document.querySelector('.mainDiv');
-const heading = document.querySelector('h1');
+const heading = mainDiv.querySelector('h1');
 
 mainDiv.addEventListener('mousemove',produceShadow);
 
 function produceShadow(e){
-    const {offsetX:x, offsetY:y} = e;
-    console.log(x,y);
+
+    const {offsetWidth:width,offsetHeight:height} = mainDiv;
+
+    let {offsetX:x,offsetY:y} = e;
+
+    let xWalk = ((x / 2.5) / width * 100);
+    let yWalk = ((y / 2) / height * 100);
+
+    if(e.target!==this){
+        x = e.offsetX  + x ;
+        y = e.offsetY + y ;
+    }
+    
+    heading.style.textShadow = 
+    `
+    ${xWalk}px ${yWalk}px red ,
+    ${-xWalk}px ${yWalk}px blue ,
+    ${xWalk}px ${-yWalk}px green ,
+    ${-xWalk}px ${-yWalk}px orange 
+    `
 }
